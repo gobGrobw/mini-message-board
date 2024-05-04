ee1614ee39a7f2911ce7174fc851605b8df193b6
gobGrobw <albertfoeng12@gmail.com>
2024-05-04 18:41:54 +0700

    fixing host

--- api/index.js
+++ api/index.js
@@ -15,14 +15,14 @@ var app = express();
 mongoose.connect(process.env.MONGO_URI);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/message', messageRouter);
@@ -42,6 +42,6 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error');
 });
-	
+
 module.exports = app;
 
